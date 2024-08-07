63cb3575f2edd5b3c6d5b9d4f5047cae59d13cc9
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2024-08-07 20:40:00 -0300

    fix: run pending migrations

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -56,6 +56,8 @@ namespace Presentation
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            DatabaseManagmentService.MigrationInitialisation(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
